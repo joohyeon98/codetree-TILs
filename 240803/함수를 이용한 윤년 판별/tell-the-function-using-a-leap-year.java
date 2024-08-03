50cb8aa0d9702248b909f00bb1490932d8ad4cad
이주현 <80081712+joohyeon98@users.noreply.github.com>
2024-08-03 23:40:06 +0900

    Title: 함수를 이용한 윤년 판별, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240803/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
+++ 240803/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -1,20 +1,34 @@
 import java.util.Scanner;
 
 public class Main {
-    public static boolean year(int n) {
-        if ((n % 4 != 0) && (n % 100 == 0)) {
+    public static boolean isLeapYear(int y) {
+        // 4의 배수가 아니라면 윤년이 확실히 아닙니다.
+        if(y % 4 != 0)
             return false;
-        }
-        if ((n % 4 == 0) && (n % 400 == 0)) {
+        
+        // 여기까지 온 이상 4의 배수임을 가정해도 됩니다.
+        // 그 중 100의 배수가 아니라면 확실히 윤년입니다.
+        if(y % 100 != 0)
             return true;
-        }
+        
+        // 여기까지 온 이상 100의 배수임을 가정해도 됩니다.
+        // 그 중 400의 배수라면 확실히 윤년입니다.
+        if(y % 400 == 0)
+            return true;
+        
+        // 여기까지 온 이상 100의 배수이지만, 400의 배수가 아닙니다.
+        // 따라서 확실히 윤년이 아닙니다.
         return false;
     }
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+        // 변수 선언 및 입력:
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
 
-        System.out.print(year(n));
+        if(isLeapYear(y))
+            System.out.println("true");
+        else
+            System.out.println("false");
     }
 }
